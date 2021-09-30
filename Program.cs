dc365e8a32a12f62e537149aaedfd308a753e9ad
TouchInTouch <35906277+IMGameDesigner@users.noreply.github.com>
2021-09-30 13:56:11 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace Excel2Csv
             //重要
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);//注册Nuget包System.Text.Encoding.CodePages中的编码到.NET Core
 
-            DirectoryInfo di = new DirectoryInfo(@"D:\\excel2csv\xls_tmp\");
+            DirectoryInfo di = new DirectoryInfo(xlsPath);
             var files = di.GetFiles("*.xlsx");
 
             foreach (var file in files)
